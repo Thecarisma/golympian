b4b34c2392dec114c2d4b229509acc73cecf55a7
Thecarisma <azeezadewale98@gmail.com>
2017-12-22 04:20:02 +0100

    First initial commit for putting the first files over github. THE FIRST

---
+++ src/main/java/gig/library/golympian/resources/Currency.java
@@ -0,0 +1,127 @@
+package gig.library.golympian.resources;
+
+/**
+ *  Created by Azeez Adewale on 11/25/2017.
+ *  Copyright Great Innovation Group
+ *  Licenced under tri Licence
+ *      MIT LICENCE
+ *      GNU LICENCE
+ *      CDDL LICENCE
+ *  on this day 11/25/2017 : 11:36 PM
+ *
+ *  Enum : Currency
+ *  Filename : Currency.java
+ *  Project : Coin World
+ *  Package : com.gig.olympus.resources
+ *  Computer Name : Thecarisma
+ */
+
+/**
+ * the currency supported in the gig GOLYMPIAN library
+ * 20 for now
+ *
+ * @author Azeez Adewale
+ */
+public enum Currency {
+
+    /**
+     *  the united state of america dollar
+     */
+    USD,
+
+    /**
+     *  the nigeria naira
+     */
+    NGN,
+
+    /**
+     *  the european union euro
+     */
+    EUR,
+
+    /**
+     *  the great britain pounds
+     */
+    GBP,
+
+    /**
+     *  the south africa rands
+     */
+    ZAR,
+
+    /**
+     *  the great indian rupees
+     */
+    INR,
+
+    /**
+     *  the canada dollay
+     */
+    CAD,
+
+    /**
+     *  the libya dollar
+     */
+    LYD,
+
+    /**
+     *  the brunei dollar
+     */
+    BND,
+
+    /**
+     *  the singapore dollar
+     */
+    SGD,
+
+    /**
+     *  the brazilian real
+     */
+    BRL,
+
+    /**
+     *  the ancient isreal shekeal
+     */
+    ILS,
+
+    /**
+     *  the magnificient russia rubble
+     */
+    RUB,
+
+    /**
+     *  the ghana cedis
+     */
+    GHS,
+
+    /**
+     *  the ancient turkey lira
+     */
+    TRY,
+
+    /**
+     *  the new zealand dollar
+     */
+    NZD,
+
+    /**
+     *  the switzerland franc
+     */
+    CHF,
+
+    /**
+     *  the cayman island dollar
+     */
+    KYD,
+
+    /**
+     *  the latvia lat
+     */
+    LVL,
+
+    /**
+     *  the jordan dinar
+     */
+    JOD
+
+}
