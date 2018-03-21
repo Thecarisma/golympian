38d7f7679c9eaab6d5cb54695559edb69adde6c6
Thecarisma <azeezadewale98@gmail.com>
2018-03-21 15:49:12 +0100

    upgrade the gradle file to remove outdated compilation flags

--- src/main/java/gig/library/golympian/base/BaseActivity.java
+++ src/main/java/gig/library/golympian/base/BaseActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import gig.library.golympian.utilities.Font;
 import gig.library.golympian.utilities.Language;
 import gig.library.golympian.utilities.Theme;
-import gig.library.golympian.views.LightView;
 
 /**
  * Created by thecarisma on 12/23/2017.
@@ -17,29 +16,20 @@ import gig.library.golympian.views.LightView;
 public class BaseActivity extends AppCompatActivity {
 
     public SharedPreferences settings ;
-    public Font font ; public static BaseActivity baseActivity ;
+    public Font font ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         settings = PreferenceManager.getDefaultSharedPreferences(this); initActivity() ;
-        baseActivity = this ;
     }
 
     public void initActivity() {
-        Theme.setTheme(this, Theme.getThemeId(settings.getInt("THEME", Theme.getThemeId(Theme.Themes.GREENLIFE))));
-        font = new Font(this);
-        Language.setLanguage(this, settings.getString("LANGUAGE",Language.getLanguage(Language.Languages.ENGLISH)));
+        Theme.setTheme(this, Theme.Themes.SKYBLUE); font = new Font(this);
+        Language.setLanguage(this, Language.Languages.ENGLISH);
         font.overrideFont(this, "SERIF", font.getSavedFont());
     }
 
-    public void setTitle(String title) {
-        if ( getSupportActionBar() != null) getSupportActionBar().setTitle(title);
-    }
-
-    public BaseActivity getActivity() {
-        return this ;
-    }
 
     //we are not using a custom shared preference becuse we want other activity that extends this class
     //to have access to the shared preference
