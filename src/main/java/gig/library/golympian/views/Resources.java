a802230373c283ae38f5d7f3d728c2806c431954
Thecarisma <azeezadewale98@gmail.com>
2018-04-03 18:24:04 +0100

    add over five loading gif url

---
+++ src/main/java/gig/library/golympian/views/Resources.java
@@ -0,0 +1,14 @@
+package gig.library.golympian.views;
+
+public class Resources {
+
+    public static String loadingUrlOne = "https://thumbs.gfycat.com/RadiantCheerfulBigmouthbass-max-1mb.gif" ;
+    public static String loadingUrlTwo = "http://gifimage.net/wp-content/uploads/2017/09/ajax-loading-gif-transparent-background-8.gif" ;
+    public static String loadingUrlThree = "http://gifimage.net/wp-content/uploads/2017/09/animated-loading-gif-transparent-background-6.gif" ;
+    public static String loadingUrlFour = "https://loading.io/spinners/coolors/lg.palette-rotating-ring-loader.gif" ;
+    public static String loadingUrlFive = "http://backgroundcheckall.com/wp-content/uploads/2017/12/loading-gif-transparent-background-12.gif" ;
+    public static String loadingUrlSix = "https://thumbs.gfycat.com/RadiantCheerfulBigmouthbass-max-1mb.gif" ;
+    public static String loadingUrlSeven = "https://thumbs.gfycat.com/RadiantCheerfulBigmouthbass-max-1mb.gif" ;
+    public static String loadingUrlEight = "https://thumbs.gfycat.com/RadiantCheerfulBigmouthbass-max-1mb.gif" ;
+
+}
