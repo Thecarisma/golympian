ad9d4789b2d7c62b67eb21bccb6be85df85d09c7
Azeez Adewale <azeezadewale98@gmail.com>
2018-08-09 18:00:14 +0100

    Merge branch 'master' of https://github.com/golympian/goandroid
    # Conflicts:
    #	build.gradle
    #	golympian.iml
    #	src/main/java/gig/library/golympian/base/BaseActivity.java
    #	src/main/res/drawable-v24/settings_one.png
    #	src/main/res/drawable/settings_one.png
    #	src/main/res/values/colors.xml

---
+++ library/src/main/java/gig/library/golympian/base/BaseActivity.java
@@ -0,0 +1,57 @@
+package gig.library.golympian.base;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.multidex.MultiDex;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+
+import gig.library.golympian.utilities.Font;
+import gig.library.golympian.utilities.Language;
+import gig.library.golympian.utilities.Theme;
+import gig.library.golympian.views.LightView;
+
+/**
+ * Created by thecarisma on 12/23/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    public SharedPreferences settings ; public Theme.Themes theme = Theme.Themes.HERATHEME;
+    public Font font ; public static BaseActivity baseActivity ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        settings = PreferenceManager.getDefaultSharedPreferences(this); initActivity() ;
+        baseActivity = this ; AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+    public void initActivity() {
+        theme = Theme.getThemeId(settings.getInt("THEME", Theme.getThemeId(Theme.Themes.HERATHEME)));
+        Theme.setTheme(this, Theme.getThemeId(settings.getInt("THEME", Theme.getThemeId(Theme.Themes.HERATHEME))));
+        font = new Font(this);
+        Language.setLanguage(this, settings.getString("LANGUAGE",Language.getLanguage(Language.Languages.ENGLISH)));
+        font.overrideFont(this, "SERIF", font.getSavedFont());
+    }
+
+    public void setTitle(String title) {
+        if ( getSupportActionBar() != null) getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base){
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    public BaseActivity getBaseActivity() {
+        return this ;
+    }
+
+    //we are not using a custom shared preference becuse we want other activity that extends this class
+    //to have access to the shared preference
+    //settings = this.getSharedPreferences(aresconstants.SHAREDPREFERENCENAME, MODE_PRIVATE);
+}
